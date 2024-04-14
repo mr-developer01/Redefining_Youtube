b7d2fa0a695c6c43e1c3cc5d18715bebf8cbcb5c
Rahul <rahulpratik1212@gmail.com>
2024-04-15 02:10:02 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCfUxsgkhKLBWpPxFMHbHbqXq0qajq2ifU";
+export const CATEGORY = "https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=";
+export const POPULAR_VIDEOS = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=0&key="
